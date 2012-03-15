a348387398baf8227b97fe032c4c6baea2892244
Anil Madhavapeddy <anil@recoil.org>
2012-03-15 14:35:51 +0000

    add more unit/stress tests for Simplex memory allocator

--- lib_test/simplex_test.ml
+++ lib_test/simplex_test.ml
@@ -33,30 +33,56 @@ let rec was_successful =
     | RTodo _::_ ->
         false
 
-let alloc_release_loop ~ring ~total_size ~alloc_size =
-  for i = 0 to 100000 do
-    match Simplex.alloc ring alloc_size with
-    |Some extent ->
-      assert_equal alloc_size (Simplex.length extent);
-      Simplex.release ring extent;
-    |None -> assert_failure "allocate failed"
-   done
+(* Given a set of ring sizes and allocation sizes to do within
+ * the ring, run all combinations with the test function *)
+let with_alloc_pattern name ~total_sizes ~alloc_sizes test =
+  List.flatten (
+    List.map (fun total_size ->
+      List.map (fun alloc_size ->
+        let shm = Shm.open_anonymous () in
+        let ring = Simplex.attach_tx shm total_size in
+        sprintf "%s_%d_%d" name total_size alloc_size >::
+        test ~ring ~total_size ~alloc_size
+      ) alloc_sizes
+    ) total_sizes
+  )
 
+(* Allocate a single value and release it repeatedly, checking
+ * that allocation never fails *)
 let allocate_release_test =
-  let fn ~total_size ~alloc_size () =
-    let shm = Shm.open_anonymous () in
-    let ring = Simplex.attach_tx shm total_size in
-    alloc_release_loop ~ring ~total_size ~alloc_size
-  in
-  (* total sizes must be page aligned *)
   let total_sizes = [4096; 8192; 16384] in
   let alloc_sizes = [1;2;3;7;8;128;256;1024;1025] in
-  List.flatten (List.map (fun total_size ->
-    List.map (fun alloc_size ->
-      sprintf "allocate_test_%d_%d" total_size alloc_size >::
-      (fn ~total_size ~alloc_size)
-    ) alloc_sizes
-  ) total_sizes)
+  with_alloc_pattern "allocate_release" ~total_sizes ~alloc_sizes
+    (fun ~ring ~total_size ~alloc_size () ->
+      for i = 0 to 100000 do
+        match Simplex.alloc ring alloc_size with
+        |Some extent ->
+          assert_equal alloc_size (Simplex.length extent);
+          Simplex.release ring extent;
+        |None -> assert_failure "allocate failed"
+       done
+    )
+
+(* Allocate until the ring is full, and then verify that it does
+ * indeed have no space left *)
+let alloc_until_full =
+  let total_sizes = [4096; 8192; 32768] in
+  let alloc_sizes = [ 128; 256; 512; 4096 ] in
+  with_alloc_pattern "allocate_until_full" ~total_sizes ~alloc_sizes
+    (fun ~ring ~total_size ~alloc_size () ->
+      let rec fill_tx acc =
+        (* allocate until we get a short alloc or a failure *)
+        match Simplex.alloc ring alloc_size with
+        |Some extent ->
+           if (Simplex.length extent) != alloc_size then acc
+           else fill_tx (acc+1)
+        |None -> acc
+      in
+      assert_bool "ring has space" (Simplex.has_free_space ring);
+      let filled = fill_tx 0 in
+      assert_equal filled (total_size / alloc_size);
+      assert_bool "ring is full" (not (Simplex.has_free_space ring))
+    )
 
 let _ =
   let suite = "Simplex" >::: allocate_release_test in
