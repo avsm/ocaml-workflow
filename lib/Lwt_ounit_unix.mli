8c373acc848a361a87802135fe2cb1a9cc2a9b81
Anil Madhavapeddy <anil@recoil.org>
2012-03-29 13:42:45 +0100

    some comments on interfaces

--- lib/Lwt_ounit_unix.mli
+++ lib/Lwt_ounit_unix.mli
@@ -20,16 +20,26 @@ type perf_ctr = {
   post_gc: Gc.stat;
 }
 
+(* Server function which is given a bound (but not listening) fd and its sockaddr,
+ * and also an integer the total number of iterations the client will perform. *)
 type server_fun = Lwt_unix.file_descr -> Lwt_unix.sockaddr -> int -> unit Lwt.t
+
+(* The client is given a connected file descriptor, and the total number of iterations
+ * it is expected to perform on that fd. *)
 type client_fun = Lwt_unix.file_descr -> Lwt_unix.sockaddr -> int -> unit Lwt.t
 
-val with_server :
-  ?ty:Lwt_unix.socket_type -> string -> int -> server_fun -> unit -> unit Lwt.t
-val with_client :
-  ?ty:Lwt_unix.socket_type -> string -> int -> client_fun -> (unit -> unit Lwt.t) Lwt.t
+(* Create a server socket, given a string socket path and a total number of expected iterations, 
+ * and a server function to process incoming connections. The result is staged with a () to
+ * curry it up as a test function *)
+val with_server : ?ty:Lwt_unix.socket_type -> string -> int -> server_fun -> unit -> unit Lwt.t
+
+val with_client : ?ty:Lwt_unix.socket_type -> string -> int -> client_fun -> (unit -> unit Lwt.t) Lwt.t
+
+(* Given a list of Lwt threads, run them in independent processes and join until all processes
+ * terminal *)
 val run_p : ('a -> 'b Lwt.t) list -> 'a -> unit
+
 type procset = { server : server_fun; clients : client_fun list; }
-val test_procset_p :
-  name:string ->
-  iters:int -> ?ty:Lwt_unix.socket_type -> procset -> string -> unit -> unit
+
+val test_procset_p : name:string -> iters:int -> ?ty:Lwt_unix.socket_type -> procset -> string -> unit -> unit
 
