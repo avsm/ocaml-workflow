ee57079940e6b6e5e4930c1f390112e2dac9c259
Anil Madhavapeddy <anil@recoil.org>
2012-03-29 23:07:12 +0100

    use the new test framework for shm_test

--- lib_test/shm_pipe_test.ml
+++ lib_test/shm_pipe_test.ml
@@ -16,58 +16,64 @@
 
 open Lwt
 open Printf
+open Lwt_ounit
+open Lwt_ounit_unix
 
 let dprintf fmt =
   let xfn ch = fprintf ch fmt in
-  kfprintf xfn stderr "[%d] " (Unix.getpid())
+  kfprintf xfn stderr "[%d] " (Unix.getpid ())
 
-let listen_t () =
-  let name = "foo" in
-  let fn h =
+let server num_clients fd sa client_iters =
+  let listen_stream = Shm_pipe.listen fd in
+  let th,u = Lwt.task () in
+  let n = ref 0 in
+  let process h =
     let rx, tx_send, tx_release, tx_close, tx_alloc = Shm_pipe.streams_of_handle h in
-    for_lwt i = 0 to 10000 do
-      let data = sprintf "%d*\n%!" i in
-      lwt ext = tx_alloc (String.length data) in
-      let buf = Simplex.buffer ext in
-      Lwt_bytes.blit_string_bytes data 0 buf 0 (String.length data);
-      tx_send ext;
-      return ()
-    done >>
-    Lwt_unix.sleep 5.0
+    lwt () =
+      for_lwt i = 0 to client_iters - 1 do
+        let data = sprintf "%d*\n%!" i in
+        lwt ext = tx_alloc (String.length data) in
+        let buf = Simplex.buffer ext in
+        Lwt_bytes.blit_string_bytes data 0 buf 0 (String.length data);
+        tx_send ext >>
+        return ()
+      done
+    in
+    tx_close () >>
+    return ()
   in
-  let listen_t = Shm_pipe.listen ~name fn in
-  listen_t
+  let listen_t = Lwt_stream.iter_s (fun h ->
+    lwt () = process h in
+    incr n;
+    if !n >= num_clients then Lwt.wakeup u ();
+    return ()
+  ) listen_stream in
+  lwt () = th in
+  Lwt.cancel listen_t;
+  return ()
 
-let connect_t () =
-  let name = "foo" in
-  lwt ch = Shm_pipe.connect ~name () in
+let client fd sockaddr num_iters =
+  lwt ch = Shm_pipe.connect fd in
   let rx, tx_send, tx_release, tx_close, tx_alloc = Shm_pipe.streams_of_handle ch in
   let t = Lwt_stream.iter_s
     (fun ext ->
-      let buf = Simplex.buffer ext in
-      dprintf "recv: %S\n%!" (Lwt_bytes.to_string buf);
-      tx_release ext;
-      return ()
+      let _ = Simplex.buffer ext in
+      tx_release ext
     ) rx 
   in
+  tx_close () >>
   t
 
-let _ = 
-  (* Fork two processes *)
-  match Lwt_unix.fork () with
-  |0 -> begin (* child *)
-    (* sleep for a bit, and then try to connect to the parent *)
-    Unix.sleep 1;
-    (try
-      Lwt_unix.run (connect_t ())
-    with Unix.Unix_error (e,_,_) as exn ->
-      dprintf "%s (%s)\n%!" (Printexc.to_string exn) (Unix.error_message e));
-    dprintf "child: exit\n%!"
-  end
-  |pid -> begin (* parent *)
-    (try
-      Lwt_unix.run (listen_t ())
-    with Unix.Unix_error (e,_,_) as exn ->
-      dprintf "%s (%s)\n%!" (Printexc.to_string exn) (Unix.error_message e));
-    dprintf "parent: exit\n%!"
-  end
+let shm_pipe_test ~rpc_iters =
+  let open Lwt_ounit_unix in
+  let clients = [ client; ] in
+  let ps = { server = server (List.length clients); clients } in
+  (* Generate a random sockpath, and do not use tempfile, as 
+   * that may be a no-exec mount point *) 
+  let sockpath = sprintf "shm_pipe_test.%d.sock" (Random.int 10000) in
+  "rpc_ping_smoke" >::= test_procset_p ~name:"rpc_ping"
+    ~ty:Lwt_unix.SOCK_DGRAM ~iters:rpc_iters ps sockpath
+
+let _ =
+  let tests = shm_pipe_test ~rpc_iters:10000 10 in
+  Lwt_ounit.main ~suite_name:"shm_pipe_test" ~tests
